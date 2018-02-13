545e44d62205012ea18f19f9ca44701cb75f3bf1
BT_1N3_01 <BT_1N3_01@BT_1N3_01.bktech.local>
2018-02-13 08:56:52 -0500

    Added ArrayMethods3 class and merge helper method

---
+++ Lab 3.3 Tyson Lawrence Period 2/src/ArrayMethods3.java
@@ -0,0 +1,58 @@
+
+public class ArrayMethods3 {
+
+	public static String[] mergeSort(String[]list1)
+	{if(list1.length<=1)
+	   {return list1;
+		
+	   }
+	 //split array into two halves.
+	int half=list1.length/2;
+	int end=list1.length;
+	String[]left=new String[0,half];
+	String[]right=new String[half,end];
+	
+	return(merge(mergeSort(left),mergeSort(right)));
+	}
+
+
+public static String[] merge(String[]list1,String[]list2)
+{String[]merged=new String[list1.length+list2.length];
+  int l=0;
+   int r=0;
+  for(int n=0;n<merged.length;n++)
+  {
+   
+      if((l<list1.length)&&(r<list2.length)&&(list1[l].compareTo(list2[r])<0))
+    	{merged[n]=list1[l];
+         l++;}
+      
+      else if((l<list1.length)&&(r<list2.length)&&(list1[l].compareTo(list2[r])>0))
+      {merged[n]=list2[r];  
+       r++;
+      }
+	  
+      
+      else if((l<list1.length)&&(r<list2.length)&&(list1[l].compareTo(list2[r])==0))
+      {merged[n]=list2[r]; 
+       merged[n+1]=list1[l];
+       r++;
+       l++;
+       n++;
+      }
+	  
+      
+      else if((l>=list1.length)&&(r<list2.length))
+	     {merged[n]=list2[r];
+		   r++;
+	     }
+	   
+      else if((r>=list2.length)&&(l<list1.length))
+       {merged[n]=list1[l];
+       l++;}
+       
+	  
+  }
+  return merged;
+}
+}
